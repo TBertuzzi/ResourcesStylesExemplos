7f0c01b8aa78b0e5e99deeaadfc9a10ff497d4ef
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-05-04 16:14:08 -0300

    Primeira versão

---
+++ ResourcesStylesExemplos/ResourcesStylesExemplos/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ResourcesStylesExemplos
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            //App.Current.Resources["backgroundColor"] = Color.White;
+            //App.Current.Resources["textColor"] = Color.Pink;
+
+            InitializeComponent();
+
+        }
+
+        void btnTemaClicked(object sender, EventArgs e)
+        {
+            App.Current.Resources["corFundo"] = Color.FromHex(txtCorFundo.Text);
+            App.Current.Resources["corTexto"] = Color.FromHex(txtCorTexto.Text);
+        }
+    }
+}
